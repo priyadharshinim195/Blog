c0b1dbeb4fef843c2a7f14c357df2416481b3a92
priyadharshinim195 <mpriyadharshini2197@gmail.com>
2026-06-15 10:28:01 +0530

    Auto-migrate on startup for Vercel

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for mysite project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
+# Auto-run migrations on startup (needed for Vercel)
+from django.core.management import call_command
+call_command('migrate', '--run-syncdb')
 
 application = get_wsgi_application()
